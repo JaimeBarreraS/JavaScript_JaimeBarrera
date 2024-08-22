3eff62f3ef72779962140b0ce11c44c07c9be9f9
JaimeBarreraS <Jaenba10@gmail.com>
2024-08-22 10:13:25 -0500

    :bug: Fix: Repair code error

--- Dia 3/main.js
+++ Dia 3/main.js
@@ -16,5 +16,20 @@ function superDigit(n,k){
     return superDigit(totalSum.toString(), 1);
 
 }
+
+console.log("Para 148 y 3:")
+console.log("1. Suma incial: 1 + 4 + 8 = 13")
+console.log("2. Multiplicacion: 13 * 3 = 39")
+console.log("3. Nueva llamada: superDigit('39',1")
+console.log("4. Suma: 3 + 9 = 12")
+console.log("5. Nueva llamada: superDigit('12',1")
+console.log("6. Suma: 1 + 2 = 3")
+console.log("7. Resultado final")
 console.log(superDigit("148", 3)); //salida 3
+console.log("Para 123 y 3:")
+console.log("1. Suma incial: 1 + 2 + 3 = 6")
+console.log("2. Multiplicacion: 6 * 3 = 18")
+console.log("3. Nueva llamada: superDigit('18',1")
+console.log("4. Suma: 1 + 8 = 9")
+console.log("5. Resultado final")
 console.log(superDigit("123", 3)); //salida 9
