62079ced8707077ed6a070048df2f917548acea2
JaimeBarreraS <Jaenba10@gmail.com>
2024-08-24 10:49:09 -0500

    :bug: New code inserted and error fixed

--- Dia 5/script.js
+++ Dia 5/script.js
@@ -16,10 +16,12 @@ function optio_menu() {
             let opcionProducto = prompt ("Selecciones una opcion:\n1. Crear\n2. Añadir\n3. Actualizar\n4. Eliminar\n0. Volver al Menu Principal");
             switch (opcionProducto){
                 case "1":
-                    console.log("Crear");
+                    console.log("Crear_product");
+                    Crear_product();
                 break;
                 case "2":
-                    console.log("Añadir");
+                    console.log("Añadir_product");
+                    
                 break;
                 case "3":
                     console.log("Actualizar");
@@ -57,6 +59,37 @@ function optio_menu() {
             Salir();
             break;
     }
+}
+function Crear_product() {
+    console.log("Crear Producto");
+
+    const dataForm = document.getElementById('data-form');
+
+    dataForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const nuevoData = {
+            "id": Date.now(),
+            "name": document.getElementById("name").value,
+            "category": document.getElementById("category").value,
+            "price": parseFloat(document.getElementById("price").value),
+            "quantityInStock": parseInt(document.getElementById("quantityInStock").value),
+            "supplierId": parseInt(document.getElementById("supplierId").value),
+        };
+
+        const response = await fetch('data.json');
+        let dataJson = await response.json();
+
+        if (Array.isArray(dataJson) && dataJson.length > 0) {
+            if (!dataJson[0].products) {
+                dataJson[0].products = [];
+            }
+            dataJson[0].products.push(nuevoData);
+            const dataUpdatedJson = JSON.stringify(dataJson, null, 2);
+            console.log('JSON actualizado:', dataUpdatedJson);
+
+        dataForm.reset();
+    }});
 }
 Menu()
 optio_menu()
