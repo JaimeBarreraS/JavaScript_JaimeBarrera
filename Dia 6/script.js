419d240dcd9c10ebaf932136997ca80ce46c50a8
JaimeBarreraS <Jaenba10@gmail.com>
2024-08-30 10:04:25 -0500

    :bug: Bug fixes

--- Dia 6/script.js
+++ Dia 6/script.js
@@ -1,31 +1,43 @@
-//let Url = ""
 
-//for (let i=1;i < 732; i++){
-    //Url = 'https://superheroapi.com/api.php/e3957a63e8a4a50a8ddd040c3d756e0d/'+String(i);
+document.addEventListener('DOMContentLoaded', () => {
+    const UrlBase = 'https://superheroapi.com/api.php/e3957a63e8a4a50a8ddd040c3d756e0d/';
+    const searchButton = document.getElementById('searchButton');
+    const heroIdInput = document.getElementById('heroId');
+    const dataTable = document.getElementById('data');
 
-    //fetch(Url)
-    //.then(res => res.json())
-    //.then(Data => {
-        //console.log(Data.id)
-    //})
-//}
-let Url =''
-    
-    for (let i=1;i < 732; i++){
-        Url = 'https://superheroapi.com/api.php/e3957a63e8a4a50a8ddd040c3d756e0d/'+String(i);
-        
+    const fetchHero = (id) => {
+        const Url = `${UrlBase}${id}`;
         fetch(Url)
-        .then (response => response.json())
-        .then (data => console.log(data))
-        .catch(error => console.log(error))
+            .then(response => response.json())
+            .then(data => {
+                updateTable([data]);
+            })
+            .catch(() => {
+                dataTable.innerHTML = `<tr><td colspan="4">Error fetching data for ID ${id}</td></tr>`;
+            });
+    };
 
-        const API = (data) => {
-            console.log(data)
-            let body = ''
-            for (let i = 0; i<data.length; i++){
-                body += `<tr><td>${data[i].id}</td></tr>`;
-            }
-            document.getElementById('data').innerHTML = body
-    }
-    }
-    
+    const updateTable = (heroes) => {
+        let body = '';
+        heroes.forEach(hero => {
+            // Construir la fila de la tabla con una imagen
+            body += `
+                <tr>
+                    <td>${hero.id}</td>
+                    <td>${hero.name}</td>
+                    <td><img src="${hero.image.url}" alt="${hero.name}" style="width: 8vw; height: auto;"></td>
+                </tr>`;
+        });
+        dataTable.innerHTML = body;
+    };
+
+    searchButton.addEventListener('click', () => {
+        const heroId = heroIdInput.value;
+        if (heroId) {
+            fetchHero(heroId);
+        } else {
+            dataTable.innerHTML = `<tr><td colspan="4">Please enter a hero ID</td></tr>`;
+        }
+    });
+});
+
